76eb1fe24643cf1fc83c9d3ee9ff39327d4159a9
張修齊 <Floyd@Floyd-MacBook-Pro.local>
2015-12-22 19:44:50 +0800

    first commit.

---
+++ BST.h
@@ -0,0 +1,38 @@
+#include <iostream>
+
+struct node{
+	int data;
+	struct node *left_subtree;
+	struct node *right_subtree;
+};
+
+struct data_invoke{//用來確認level-order的array裡的值是否為有效值
+	int data;
+	bool invoke;
+};
+
+typedef struct node leaf;
+typedef struct data_invoke data_invoke;
+
+class BST{
+public:
+	BST();
+	bool insert(int data);
+	bool Delete(int data);
+	void printBST();
+	void printInLevel();
+	void computerize(int data);
+	int TreeDegree();
+
+private:
+	static void printBST(leaf *node);
+	static bool IsExistData(leaf *node, int data);
+	static leaf* InsertRecursion(leaf *node, int data);
+	static leaf* deleteRecursion(leaf *node, int data);
+	static leaf* GetMax(leaf *node);
+	static void printInLevel(data_invoke *a, leaf *node, int n);
+	static leaf* GetNode(leaf *node, int data);
+	static void computerize(int *result, leaf *node);
+	static void TreeDegree(leaf *node, int *max, int degree);
+	leaf *root;
+};
---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "BST.h"
+
+int main(){
+	BST mpq;
+	int temp, option;
+	std::string input;
+	bool erro;
+	std::cout << "Please enter several number seperated by space and press enter to end it up.\n";
+	getline( std::cin, input);
+	std::stringstream ss(input);
+	while( ss ){
+		ss >> temp;
+		mpq.insert(temp);
+	}
+	do{
+		std::cout << "======================================================================\n";
+		std::cout << "Menu:\n";
+		std::cout << "\t(1)Insert a new number.\n";
+		std::cout << "\t(2)Delete a number.\n";
+		std::cout << "\t(3)Computerize the total of elements in subtree(inclusive).\n";
+		std::cout << "\t(4)Exit\n";
+		std::cout << "======================================================================\n";
+		std::cout << "What do you want to do :";
+		std::cin >> option;
+		switch (option){
+			case 1:
+				std::cout << "What number you want to insert: ";
+				std::cin >> temp;
+				erro = mpq.insert(temp);
+				if(erro){
+					std::cout << "Complete.\n";
+					mpq.printInLevel();
+				}
+				else{
+					std::cout << "Error : \"Number is exist\"\n";
+					mpq.printInLevel();
+				}
+				break;
+			case 2:
+				std::cout << "What number you want to delete: ";
+				std::cin >> temp;
+				erro = mpq.Delete(temp);
+				if(erro){
+					std::cout << "Complete.\n";
+					mpq.printInLevel();
+				}
+				else{
+					std::cout << "Error : \"Number is not exist\"\n";
+					mpq.printInLevel();
+				}
+				break;
+			case 3:
+				std::cout << "What number you want to computerize it subtree: ";
+				std::cin >> temp;
+				mpq.computerize(temp);
+				mpq.printInLevel();
+				break;
+			case 4:
+				mpq.printInLevel();
+				std::cout << "Exit\n";
+				break;
+		}
+	}while( option != 4);
+}
